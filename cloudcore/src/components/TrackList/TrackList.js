ae72b2a2863f51e39ccf8b1b10d96b24af94e839
Ali Raza <aliatwork@hotmail.co.uk>
2023-04-16 21:16:38 +0100

    v0.2

--- cloudcore/src/components/TrackList/TrackList.js
+++ cloudcore/src/components/TrackList/TrackList.js
@@ -1,3 +1,4 @@
+// components/Tracklist/Tracklist.js
 import React, { useState } from 'react';
 import './TrackList.scss';
 import Track from '../Track/Track';
@@ -13,14 +14,18 @@ const TrackList = () => {
     setTracks([...tracks, { id: Date.now(), type: 'midi' }]);
   };
 
+  const handleDeleteTrack = (id) => {
+    setTracks(tracks.filter((track) => track.id !== id));
+  };
+
   return (
     <div className="track-list">
-      <div className="add-track-buttons"> {/* Add a container div */}
+      <div className="add-track-buttons">
         <button onClick={addAudioTrack}>Add Audio Track</button>
         <button onClick={addMidiTrack}>Add MIDI Track</button>
       </div>
       {tracks.map((track) => (
-        <Track key={track.id} type={track.type} />
+        <Track key={track.id} type={track.type} onDelete={() => handleDeleteTrack(track.id)} />
       ))}
     </div>
   );
