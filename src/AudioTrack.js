9ca8ad5a8a5e39138205afe6d2b86d9cdddd91c8
Ali Raza <aliatwork@hotmail.co.uk>
2023-05-08 16:47:18 +0100

    v1 complete

--- src/AudioTrack.js
+++ src/AudioTrack.js
@@ -1,67 +1,134 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Howl } from "howler";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+
 
 const AudioTrack = ({ onDelete, onRef }) => {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [isAudioUploaded, setIsAudioUploaded] = useState(false);
+  const [isLooping, setIsLooping] = useState(false);
+  const [volume, setVolume] = useState(1);
   const audioRef = useRef(null);
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (onRef) {
       onRef(audioRef);
     }
   }, [onRef]);
 
-  const playAudio = () => {
-    if (!audioRef.current) return;
-
-    setIsPlaying(true);
-    audioRef.current.play();
+  const play = () => {
+    if (audioRef.current) {
+      if (isPlaying) {
+        audioRef.current.pause();
+      } else {
+        audioRef.current.play();
+      }
+      setIsPlaying(!isPlaying);
+    }
   };
 
-  const stopAudio = () => {
-    if (!audioRef.current) return;
-
-    setIsPlaying(false);
-    audioRef.current.stop();
+  const stop = () => {
+    if (audioRef.current) {
+      audioRef.current.stop();
+      setIsPlaying(false);
+    }
   };
 
-  const handleFileUpload = (e) => {
-    if (!e.target.files || e.target.files.length === 0) return;
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const objectURL = URL.createObjectURL(file);
+      const fileExtension = file.name.split(".").pop().toLowerCase();
+      audioRef.current = new Howl({
+        src: [objectURL],
+        format: [fileExtension],
+        loop: isLooping,
+        onend: () => setIsPlaying(false),
+      });
+      setIsAudioUploaded(true);
+    }
+  };
 
-    const file = e.target.files[0];
-    const reader = new FileReader();
-    reader.onload = () => {
-      const audio = new Howl({ src: [reader.result] });
-      audioRef.current = audio;
-    };
-    reader.readAsDataURL(file);
+  const toggleLoop = () => {
+    setIsLooping(!isLooping);
+    if (audioRef.current) {
+      audioRef.current.loop(!isLooping);
+    }
+  };
+
+  const handleVolumeChange = (event) => {
+    const newVolume = parseFloat(event.target.value);
+    setVolume(newVolume);
+    if (audioRef.current) {
+      audioRef.current.volume(newVolume);
+    }
   };
 
   return (
-    <Box>
-      <input
-        accept="audio/*"
-        id="import-audio"
-        type="file"
-        hidden
-        onChange={handleFileUpload}
-      />
-      <label htmlFor="import-audio">
-        <Button variant="contained" component="span">
-          Import Audio
-        </Button>
-      </label>
-      <Button variant="contained" onClick={playAudio} disabled={!audioRef.current}>
-        Play
-      </Button>
-      <Button variant="contained" onClick={stopAudio} disabled={!audioRef.current}>
-        Stop
-      </Button>
-      <Button variant="contained" color="error" onClick={onDelete}>
-        X
-      </Button>
+    <Box
+      sx={{
+        border: 1,
+        borderColor: "grey.500",
+        borderRadius: 1,
+        p: 1,
+        m: 1,
+      }}
+    >
+      <Grid container alignItems="center" justifyContent="center" spacing={2}>
+        <Grid item>
+          <Button variant="contained" onClick={play}>
+            {isPlaying ? "Pause" : "Play"}
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" onClick={stop}>
+            Stop
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button
+            component="label"
+            variant="contained"
+            style={{
+              backgroundColor: isAudioUploaded ? "green" : "red",
+              color: "white",
+            }}
+          >
+            Import Audio
+            <input
+              type="file"
+              accept="audio/*"
+              hidden
+              onChange={handleFileChange}
+            />
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" onClick={onDelete}>
+            X
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" onClick={toggleLoop}>
+            {isLooping ? "Disable Loop" : "Enable Loop"}
+          </Button>
+        </Grid>
+        <Grid item>
+          <Box component="label" display="flex" flexDirection="column">
+            <span>Volume</span>
+            <input
+              type="range"
+              min="0"
+              max="1"
+              step="0.01"
+              value={volume}
+              onChange={handleVolumeChange}
+            />
+          </Box>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
--- src/TrackContainer.js
+++ src/TrackContainer.js
@@ -1,19 +1,26 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
-import AudioTrack from "./AudioTrack";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import AudioTrack from "./AudioTrack";
+
 
 const TrackContainer = () => {
   const [tracks, setTracks] = useState([]);
+  const [allTracksPlaying, setAllTracksPlaying] = useState(false);
 
   const addTrack = () => {
-    setTracks((prevTracks) => [...prevTracks, { id: uuidv4() }]);
+    setTracks((prevTracks) => [...prevTracks, { id: uuidv4(), audioRef: React.createRef() }]);
   };
 
   const deleteTrack = (id) => {
+    const trackToDelete = tracks.find((track) => track.id === id);
+    if (trackToDelete && trackToDelete.audioRef.current) {
+      trackToDelete.audioRef.current.stop();
+    }
     setTracks((prevTracks) => prevTracks.filter((track) => track.id !== id));
-  };
+  };  
 
   const playAllTracks = () => {
     tracks.forEach((track) => track.audioRef.current?.play());
@@ -23,32 +30,45 @@ const TrackContainer = () => {
     tracks.forEach((track) => track.audioRef.current?.stop());
   };
 
+  const togglePlayAll = () => {
+    if (allTracksPlaying) {
+      stopAllTracks();
+    } else {
+      playAllTracks();
+    }
+    setAllTracksPlaying(!allTracksPlaying);
+  };  
+
   return (
-    <Box>
+    <Box
+      sx={{
+        border: 1,
+        borderColor: "grey.500",
+        borderRadius: 1,
+        p: 1,
+        m: 1,
+      }}
+    >
+      <Grid container alignItems="center" justifyContent="center" spacing={2}>
+        <Grid item>
+          <Button variant="contained" onClick={togglePlayAll}>
+            {allTracksPlaying ? "Stop All" : "Play All"}
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" onClick={addTrack}>
+            Add Track
+          </Button>
+        </Grid>
+      </Grid>
       <Box>
-        <Button variant="contained" onClick={playAllTracks}>
-          Master Play
-        </Button>
-        <Button variant="contained" onClick={stopAllTracks}>
-          Master Stop
-        </Button>
-        <Button variant="contained" onClick={addTrack}>
-          Add Track
-        </Button>
-      </Box>
-      <Box>
-        {tracks.map((track) => (
-          <AudioTrack
-            key={track.id}
-            onDelete={() => deleteTrack(track.id)}
-            onRef={(ref) => {
-              track.audioRef = ref;
-            }}
-          />
+        {tracks.map((track, index) => (
+          <AudioTrack key={index} onDelete={() => deleteTrack(index)} />
         ))}
       </Box>
     </Box>
-  );
+  );  
+   
 };
 
 export default TrackContainer;
